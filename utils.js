1d49b605991aae6c89520f560e7d0769e5b02c1f
koyahness <cryptomakoya@gmail.com>
2026-02-28 13:09:27 +0200

    update the utils.js file

--- utils.js
+++ utils.js
@@ -2,5 +2,7 @@
 // file with functions/ modules
 
 function generateRandomNumber() {
-    
+    return Math.floor(Math.random() * 100) + 1;
 }
+
+module.exports = generateRandomNumber; //since its exported, it can be imported in other files
