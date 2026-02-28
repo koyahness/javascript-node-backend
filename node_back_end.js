d1b973fc105f450356de2fb9a544f58e90afb743
koyahness <cryptomakoya@gmail.com>
2026-02-28 12:00:53 +0200

    and node.js coding file initial comments

---
+++ node_back_end.js
@@ -0,0 +1,19 @@
+/*
+Prerequisites:
+
+Javascript fundamentals (functions, loops, objects, classes etc)
+Asynchronous programming - promises, callbacks, async/await
+HTTP basics (methods, status codes etc)
+How JSON APIs work
+NPM (node package manager)
+
+V8 JS engine: node is built on V8 JS engine (that is written in C++) the same engine that powers google chrome. Other browsers have their own engines like on firefox has Spider MOnkey, safari has  javascript core
+
+Non-blocking
+
+single threaded
+
+event loop
+
+*/
+
