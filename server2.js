7a3df273e143a9f1f6a3ab3d273d303e947a5913
koyahness <cryptomakoya@gmail.com>
2026-03-02 18:11:16 +0200

    update api in server 2

--- server2.js
+++ server2.js
@@ -1,8 +1,10 @@
-
-
-import http from 'http';
+import http, { request } from 'http';
 import { json } from 'stream/consumers';
 const PORT = 8000;
+
+//APIs commony return data in JSON format (Javascript Object Notation) other formats are csv
+//RESTful APIs commony return data in JSON format (Javascript Object Notation)
+//data will be from API endpoints, public APIs, external sources, your own backend/ database eg /users. /products
 
 const users = [
     {id: 1, name: 'Koyah'},
@@ -10,13 +12,33 @@ const users = [
     {id: 3, name: 'Koyzile'},
 ];
 
+// logger middleware... middleware are functions handling requests, such as auth, logging
+const logger = (req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+};
+
 const server = http.createServer( (req, res) => {
     if (req.url === '/api/users' && req.method === 'GET') {
         res.setHeader('Content-Type', 'application/json'); //another option is text/plain
         res.write(json.stringify(users)); //pass in the data
         res.end();
     } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-         res.write(json.stringify({id: 1, name: 'Koyah'})); //pass in the data
+        const id = req.url.split('/')[3];
+        console.log(id);
+        const user = user.find((user) => user.id === parseInt(id));
+
+        if (user) {
+             res.setHeader('Content-Type', 'application/json'); //another option is text/plain
+             res.write(json.stringify({user})); //pass in the data
+             res.end();
+        } else {
+            res.setHeader('Content-Type', 'application/json'); //another option is text/plain
+            res.statusCode = 404;
+            res.write(json.stringify({message: 'User not found'})); //pass in the data
+            res.end();
+        }
+
     } else {
         res.setHeader('Content-Type', 'application/json'); //another option is text/plain
         res.statusCode = 404;
