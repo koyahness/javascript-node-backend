e2e43a291a6ef1ce83a08adda41b9588336a40ba
koyahness <cryptomakoya@gmail.com>
2026-03-07 17:24:25 +0200

    update the osdemo.js file

---
+++ osDemo.js
@@ -0,0 +1,14 @@
+//gives information about the system
+import os from 'os'
+
+//userinfo
+console.log(os.userInfo())
+
+//total memory
+console.log(os.totalmem())
+
+//freemem
+console.log(os.freemem())
+
+//cpus
+console.log(os.cpus())
