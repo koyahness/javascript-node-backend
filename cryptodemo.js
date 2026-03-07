721b8b741200d26db8333f368da4785f106110d2
koyahness <cryptomakoya@gmail.com>
2026-03-07 18:35:30 +0200

    add crypto demo file

--- cryptodemo.js
+++ cryptodemo.js
@@ -5,9 +5,24 @@
 
 import crypto from 'crypto'
 
+crypto.cre
 // create hash with createhash method
-const hash = crypto.hash('sha256'); //take in the algorithm you want to use
+const hash = crypto.createHash('sha256'); //take in the algorithm you want to use
+  
 hash.update('password1234') //to hash something use the update method
- console.log(hash.digeest) // to get the hash
+
+//to never store plain text passwords in databases, so hash it
+
+ console.log(hash.digest('hex')) // to get the hash... set output to be in hex
+
+//to generate cryptographically strong hexadecimalsring
+
+crypto.randomBytes(16, (err, buff) => {
+    if (err) throw err;
+    console.log(buff.toString('hex'));
+})
+//can use to generated userids for exampl
+
+
 
 
