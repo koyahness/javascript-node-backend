13b610adf94140f8f2d9e5f8d289dee0ae9291bb
koyahness <cryptomakoya@gmail.com>
2026-03-07 17:03:20 +0200

    add path module file

---
+++ pathdemo.js
@@ -0,0 +1,13 @@
+import path from 'path';
+
+const filePath = './dir1/dir2/test.txt';
+
+
+// basename() will gv text.txt
+console.log(path.basename(filePath));
+
+// dirname() will give the directory names
+console.log(path.dirname(filePath));
+
+// get the extension name of the file
+console.log(path.extname(filePath));
