746210e8f7f1ce00f5ac4539e678491538ff3cb9
koyahness <cryptomakoya@gmail.com>
2026-03-07 16:06:27 +0200

    update js files

--- fsDemo.js
+++ fsDemo.js
@@ -1,8 +1,20 @@
-import fs from 'fs';
+// import fs from 'fs';
+import fs from 'fs/promises';
 
-// readFile() - Callback
-fs.readFile('./test.txt', 'UTF8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+// // readFile() - Callback
+// fs.readFile('./test.txt', 'UTF8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+// // readFileSync() - Synchronous
+// const data = fs.readFileSync('./test.txt', 'utf8');
+//  console.log(data);
+
+
+ // readFile() - promise version .then( syntax) 
+fs.readFile('./test.txt', 'utf8'); // this returns a promises
+.then((data) => console.log(data));
+.catch((err) => console.log(err));
 
