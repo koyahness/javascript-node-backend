255a5e2e63442c4cd38e55476b9f844489a000ee
koyahness <cryptomakoya@gmail.com>
2026-03-07 17:53:19 +0200

    update the urldemo.js file

--- urldemo.js
+++ urldemo.js
@@ -15,4 +15,12 @@ console.log(import.meta.url)
 //fileURLToPath
 console.log(url.fileURLToPath(import.meta.url))
 
+console.log(urlObj.search);
+
+const params = new URLSearchParams(urlObj.search);
+console.log(params.get('q'));
+params.append('limit', '5');
+params.delete('limit');
+console.log(params)
+
 
